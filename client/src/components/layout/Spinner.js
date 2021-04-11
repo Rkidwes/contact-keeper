068f613056cc64f022bfb61ed315dbbf23c240ea
Rkidwes <75211739+Rkidwes@users.noreply.github.com>
2021-04-11 10:58:01 +0100

    Build finished

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,12 +1,9 @@
-import { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export default () => {
-    <Fragment>
-        <img
-            src={spinner}
-            style={{ width: '200px', margin: 'auto', display: 'block' }}
-            alt='Loading...'
-        />
-    </Fragment>
-};
+export default () => (
+    <img
+        src={spinner}
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+        alt='Loading...'
+    />
+);
